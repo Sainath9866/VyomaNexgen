4e09f2363b57ac6495bc118fc3b7b2b445378883
Sainath Reddy <cvsainath9866@gmail.com>
2025-08-17 01:22:32 +0530

    enabled nextjs for images

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,12 @@ const nextConfig: NextConfig = {
     // This will ignore ESLint errors during builds
     ignoreDuringBuilds: true,
   },
+  images: {
+    domains: [
+      'www.vyomanexgen.com',
+      'intileo.com'
+    ],
+  },
 };
 
 export default nextConfig;
